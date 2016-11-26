899e060e058da7d9317514dc3d3ec8ff7eeeebc7
Meghneel Gore <meghneel.gore@salesforce.com>
2016-11-26 10:38:21 -0800

    Made TypeAheadSearch return a Set instead of a List of words. Also added java docs to the com.example.trie package.

--- src/com/example/trie/TrieTest.java
+++ src/com/example/trie/TrieTest.java
@@ -1,11 +1,14 @@
 package com.example.trie;
 
 import java.util.Arrays;
-import java.util.List;
+
+import java.util.Set;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.google.common.collect.ImmutableList;
+
+import com.google.common.collect.ImmutableSet;
 
 public class TrieTest {
 
@@ -14,8 +17,8 @@ public class TrieTest {
 		Trie trie = new Trie(Arrays.asList(new String[]{"arkansas", "alabama", "california", "new hampshire", "new york"}));
 		TypeAheadSearch search = new TypeAheadSearch(trie);
 		
-		List<String> typeAhead = search.typeAheadSearch("new");
-		List<String> expectedList = ImmutableList.of("new york", "new hampshire");
+		Set<String> typeAhead = search.typeAheadSearch("new");
+		Set<String> expectedList = ImmutableSet.of("new hampshire", "new york");
 		Assert.assertEquals("Type ahead search gave wrong result", expectedList, typeAhead);
 	}
 
--- src/com/example/trie/TypeAheadSearch.java
+++ src/com/example/trie/TypeAheadSearch.java
@@ -1,9 +1,8 @@
 package com.example.trie;
 
 import java.util.LinkedList;
-import java.util.List;
-
-import com.google.common.collect.ImmutableList;
+import java.util.Set;
+import com.google.common.collect.ImmutableSet;
 
 /**
  * Class TypeAheadSearch. Takes a Trie object and can perform type ahead search on it.
@@ -14,24 +13,43 @@ public class TypeAheadSearch {
 
 	private final Trie trie;
 	
+	/**
+	 * Constructor. Takes a Trie object on which it can perform type ahead search.
+	 * @param trie The Trie object on which to perform the search.
+	 */
 	public TypeAheadSearch(Trie trie) {
 		this.trie = trie;
 	}
 	
-	public List<String> typeAheadSearch(String query) {
+	/**
+	 * Performs type ahead search on a Trie object. 
+	 * 
+	 * @param query Query string
+	 * 
+	 * @return Set of all the strings in the Trie that match the query string. Returns an empty
+	 * set if nothing matches
+	 */
+	public Set<String> typeAheadSearch(String query) {
 		LinkedList<String> list = new LinkedList<>();
 		TrieNode current = trie.getHead();
 		for(Character c: query.toCharArray()) {
 			if(current.next.get(c) != null) {
 				current = current.next.get(c);
 			} else {
-				return ImmutableList.of(); //Empty list
+				return ImmutableSet.of(); //Empty set
 			}
 		}
 		searchAhead(trie.getHead().next.get(query.charAt(0)), "", list);
-		return ImmutableList.copyOf(list);
+		return ImmutableSet.copyOf(list);
 	}
 
+	/**
+	 * Helper method to search through the Trie. This method is recursive.
+	 * 
+	 * @param current Current TrieNode
+	 * @param currentString Current string that has been accumulated
+	 * @param list List of all the words found until now.
+	 */
 	private void searchAhead(TrieNode current, String currentString, LinkedList<String> list) {
 		if(current.index) {
 			list.add(currentString + current.c);
